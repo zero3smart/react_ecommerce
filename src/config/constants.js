3e110aab75c5744e700f6f523621a6a2a782c902
CodeRiderz <chiputera@gmail.com>
2018-08-01 16:30:36 +0700

    Configure axios defaults base path. Add constants.js for project default variables.

---
+++ src/config/constants.js
@@ -0,0 +1,5 @@
+// api
+export const BASE_API_PATH = process.env.REACT_APP_BASE_API_PATH || '/'
+
+// products
+export const PRODUCT_COUNT_PER_PAGE = process.env.REACT_APP_PRODUCT_PER_PAGE || 10
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
+import { BASE_API_PATH } from 'config/constants'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+
+// configure axios
+axios.defaults.baseURL = BASE_API_PATH
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
