f35f46dc0701545f8cddf2078ff720b24cdc10fe
CodeRiderz <chiputera@gmail.com>
2018-08-17 10:32:15 +0700

    add related products to TopSingle

--- desktop/src/modules/tops/TopSingle.js
+++ desktop/src/modules/tops/TopSingle.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Product, ProductPlaceholder, ProductList } from 'yesplz@modules/products'
-import { fetchProduct, fetchRelatedProducts, resetProduct, likeProduct, unlikeProduct, setScrollBellowTheFold } from 'yesplz@ducks/product'
-import { history } from 'config/store'
+import { Product, ProductPlaceholder, ProductGrid } from 'yesplz@modules/products'
+import { fetchProduct, fetchRelatedProducts, resetProduct, likeProduct, unlikeProduct } from 'yesplz@ducks/product'
+import Transition from 'yesplz@ui-kits/transitions/Transition'
 import './top-single.css'
 
 class TopSingle extends Component {
@@ -13,14 +13,11 @@ class TopSingle extends Component {
     productId: PropTypes.string.isRequired,
     product: PropTypes.object.isRequired,
     relatedProducts: PropTypes.array.isRequired,
-    nextPage: PropTypes.number,
-    scrollBellowTheFold: PropTypes.bool,
     fetchProduct: PropTypes.func.isRequired,
     fetchRelatedProducts: PropTypes.func.isRequired,
     resetProduct: PropTypes.func.isRequired,
     likeProduct: PropTypes.func.isRequired,
-    unlikeProduct: PropTypes.func.isRequired,
-    setScrollBellowTheFold: PropTypes.func.isRequired
+    unlikeProduct: PropTypes.func.isRequired
   }
 
   componentDidMount () {
@@ -77,19 +74,8 @@ class TopSingle extends Component {
     }
   }
 
-  get handleScrollBellowTheFold () {
-    const { scrollBellowTheFold, setScrollBellowTheFold } = this.props
-    return (scrollState) => {
-      // boolean can only be compared by casting it to string (js)
-      if (scrollState.toString() !== scrollBellowTheFold.toString()) {
-        history.push('#')
-        setScrollBellowTheFold(scrollState)
-      }
-    }
-  }
-
   render () {
-    const { product, relatedProducts, isProductFetched, isRelatedProductsFetched, nextPage } = this.props
+    const { product, relatedProducts, isProductFetched, isRelatedProductsFetched } = this.props
     let productBox = <ProductPlaceholder />
 
     if (isProductFetched) {
@@ -111,18 +97,32 @@ class TopSingle extends Component {
     }
 
     return (
-      <div className='TopSingle'>
+      <div id='MainScroll' className='TopSingle'>
         {productBox}
-        <ProductList
-          id='MainScroll'
-          show={isRelatedProductsFetched}
-          products={relatedProducts}
-          nextPage={nextPage}
-          onFetch={this.handleFetchNext}
-          onToggleLike={this.toggleProductLike}
-          onScrollBellowTheFold={this.handleScrollBellowTheFold}
-          className='TopSingle-products'
-        />
+        <h2>You might also like this.</h2>
+        <div className='TopSingle-relateds'>
+          <Transition show={isRelatedProductsFetched} transition='fadeInUp'>
+            {
+              relatedProducts.map((product, index) => (
+                <ProductGrid
+                  key={product.product_id}
+                  id={product.product_id}
+                  name={product.name}
+                  brand={product.brand}
+                  price={product.price}
+                  originalPrice={product.original_price}
+                  favorite={product.favorite}
+                  imgSrc={product.front_img}
+                  onToggleLike={this.toggleProductLike}
+                  style={{
+                    // `ProducGrid` need be showed directly in each page
+                    animationDelay: `${50 * index}ms`
+                  }}
+                />
+              ))
+            }
+          </Transition>
+        </div>
       </div>
     )
   }
@@ -133,9 +133,7 @@ const mapStateToProps = (state, props) => ({
   productId: props.match.params.productId,
   isProductFetched: state.product.fetched,
   isRelatedProductsFetched: state.product.relatedProductsFetched,
-  relatedProducts: state.product.relatedProducts,
-  nextPage: state.product.nextPage,
-  scrollBellowTheFold: state.product.scrollBellowTheFold
+  relatedProducts: (state.product.relatedProducts || []).slice(0, 5)
 })
 
 export default connect(
@@ -145,7 +143,6 @@ export default connect(
     fetchRelatedProducts,
     resetProduct,
     likeProduct,
-    unlikeProduct,
-    setScrollBellowTheFold
+    unlikeProduct
   }
 )(TopSingle)
