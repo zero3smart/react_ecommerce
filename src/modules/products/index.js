b8f4611df1daeae17b166cf7160cf1f38acb1249
CodeRiderz <chiputera@gmail.com>
2018-08-18 23:34:50 +0700

    add ProductGridCompact for desktop product favorite grids

--- src/modules/products/index.js
+++ src/modules/products/index.js
@@ -1,11 +1,13 @@
 import Product from './Product'
 import ProductGrid from './ProductGrid'
+import ProductGridCompact from './ProductGridCompact'
 import ProductPlaceholder from './ProductPlaceholder'
 import ProductList from './ProductList'
 
 export {
   Product,
   ProductGrid,
+  ProductGridCompact,
   ProductPlaceholder,
   ProductList
 }
