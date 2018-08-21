71c2a31208d7f4291b7b04bf87b9f43a833f993c
CodeRiderz <chiputera@gmail.com>
2018-08-21 16:14:07 +0700

    fix #43 --- root cause: Duplicate svg highlight source ids, make Safari unable to access it.
    solutions:
    separate highlight source in different file, load it once during initial load.

--- desktop/src/index.js
+++ desktop/src/index.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { BASE_API_PATH } from 'yesplz@config/constants'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import { loadSvgSource } from 'yesplz@utils/svgHelpers'
 
 // configure axios
 axios.defaults.baseURL = BASE_API_PATH
@@ -11,3 +12,6 @@ axios.defaults.baseURL = BASE_API_PATH
 ReactDOM.render(<App />, document.getElementById('root'))
 
 registerServiceWorker()
+
+// load visual filter sources
+loadSvgSource('#svg-filters', `${process.env.PUBLIC_URL}/svg/highlight.svg`)
