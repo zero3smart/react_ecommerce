9d1e58fa7bd10b48a277766c997cb8b073b93243
CodeRiderz <chiputera@gmail.com>
2018-09-18 19:40:23 +0700

    add preset favorite changes action to recommendation middleware

--- src/middlewares/recommendation.js
+++ src/middlewares/recommendation.js
@@ -1,12 +1,13 @@
 import includes from 'lodash-es/includes'
 import { fetchRecommendedProducts } from 'ducks/products'
 import { LIKE_PRODUCT, UNLIKE_PRODUCT } from 'ducks/product'
+import { LIKE_PRESET, UNLIKE_PRESET } from 'ducks/filters'
 
 const recommendation = store => next => action => {
   const { type } = action
 
   // when products is liked / unlike, re-fetch the recommended products
-  if (includes([LIKE_PRODUCT, UNLIKE_PRODUCT], type)) {
+  if (includes([LIKE_PRODUCT, UNLIKE_PRODUCT, LIKE_PRESET, UNLIKE_PRESET], type)) {
     store.dispatch(fetchRecommendedProducts())
   }
 
