3c3d3a87548b45d0836dd21870a24fa7506fde1f
CodeRiderz <chiputera@gmail.com>
2018-09-25 22:02:33 +0700

    - share questions between desktop and mobile Faq - make sure plus "+" button will always colorized when opened

--- desktop/src/modules/faq/Faq.js
+++ desktop/src/modules/faq/Faq.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { compose } from 'redux'
 import PropTypes from 'prop-types'
+import includes from 'lodash-es/includes'
+import without from 'lodash-es/without'
 import withTrackingProvider from 'yesplz@hoc/withTrackingProvider'
 import { InfoBanner } from 'yesplz@ui-kits/banners'
 import { withStyles } from '@material-ui/core/styles'
@@ -10,7 +12,7 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import Typography from '@material-ui/core/Typography'
 import IconOpenSrc from 'yesplz@assets/svg/accordion-circle-open.svg'
 import IconClosedSrc from 'yesplz@assets/svg/accordion-circle-closed.svg'
-import instagramSvgSrc from 'yesplz@assets/svg/instagram.svg'
+import questions from 'yesplz@modules/faq/questions'
 import './faq.css'
 
 class Faq extends Component {
@@ -21,50 +23,27 @@ class Faq extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      activeIndex: -1
+      activePanels: []
     }
-  }
-
-  get faqs () {
-    return [
-      // {
-      //   title: 'Who’s the team YesPlz?',
-      //   content: ''
-      // },
-      {
-        title: 'I am social, where do I find you?',
-        content: `Glad to hear that! We are on Instagram find us #yesplz_fashion Look forward to seeing you there!
-        <br/><br/>
-        <a href='https://www.instagram.com/yesplz_fashion/' target='_blank' rel='noopener noreferrer' class='SocialLink'>
-          Follow Us <img src='${instagramSvgSrc}' alt='Yesplz Instagram' />
-        </a>
-        `
-      },
-      {
-        title: 'I want to give you my feedback, how do I contact you?',
-        content: 'First, thank you! We are always looking for feedbacks to make your experience better. Simply email us to <a href="mailto:hello@yesplz.us">hello@yesplz.us</a>'
-      }
-      // {
-      //   title: 'I need the instruction again, where can I find it?',
-      //   content: 'Look no further. Here we have the instructions how to use our search filters. Please email us at mailto:hello@yesplz.us if you can’t find the right instruction, we will reply back in 24 hours.'
-      // }
-    ]
   }
 
   makeAccordionChangeHandler (key) {
     return (_, expanded) => {
-      let activeIndex = -1
+      const { activePanels } = this.state
+      let nextActivePanels = activePanels
       if (expanded) {
-        activeIndex = key
+        nextActivePanels = [...activePanels, key]
+      } else {
+        nextActivePanels = without(activePanels, key)
       }
 
-      this.setState({ activeIndex })
+      this.setState({ activePanels: nextActivePanels })
     }
   }
 
   render () {
     const { classes } = this.props
-    const { activeIndex } = this.state
+    const { activePanels } = this.state
 
     return (
       <div id='MainScroll' className='Faq'>
@@ -77,10 +56,10 @@ class Faq extends Component {
         <div className='faqs-questions container'>
           <div className={classes.root}>
             {
-              this.faqs.map((faq, index) => (
+              questions.map((faq, index) => (
                 <ExpansionPanel key={index} className={classes.panel} onChange={this.makeAccordionChangeHandler(index)}>
                   <ExpansionPanelSummary
-                    expandIcon={<img src={activeIndex === index ? IconOpenSrc : IconClosedSrc} />}
+                    expandIcon={<img src={includes(activePanels, index) ? IconOpenSrc : IconClosedSrc} />}
                     className={classes.headingWrapper}
                     classes={{ expanded: 'expanded', expandIcon: 'expandIcon' }}>
                     <Typography className={classes.heading}>
