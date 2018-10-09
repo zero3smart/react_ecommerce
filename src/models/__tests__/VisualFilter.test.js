a0b796f2fd8169b382d43a4b920d15488e1408d1
CodeRiderz <chiputera@gmail.com>
2018-10-09 11:23:10 +0700

    - onboarding: rewire delayed function, invoke test manually instead - add VisualFilter `changePropSelection` test

--- src/models/__tests__/VisualFilter.test.js
+++ src/models/__tests__/VisualFilter.test.js
@@ -1,12 +1,12 @@
 import { expect } from 'chai'
 import sinon from 'sinon'
 import each from 'lodash-es/each'
-import delay from 'utils/delay'
 import VisualFilter from '../VisualFilter'
 
 describe('VisualFilter Onboarding', () => {
   let vf = null
-  const fakeFn = {
+  let contructorStub
+  let fakeFn = {
     showGroup: sinon.spy(),
     hideGroup: sinon.spy(),
     changePropSelection: sinon.spy(),
@@ -17,11 +17,9 @@ describe('VisualFilter Onboarding', () => {
   }
 
   before(() => {
-    sinon.stub(VisualFilter, 'constructor').callsFake(() => {})
-    // will stub VisualFilter with all function from fakeFn
-    each(fakeFn, (spy, name) => {
-      sinon.stub(VisualFilter.prototype, name).callsFake(spy)
-    })
+    VisualFilter.__Rewire__('setTimeout', () => {}) // don't run set delayed func
+    contructorStub = sinon.stub(VisualFilter, 'constructor').callsFake(() => {})
+    stubFakeFunctions(VisualFilter, fakeFn)
 
     vf = new VisualFilter()
   })
@@ -84,10 +82,9 @@ describe('VisualFilter Onboarding', () => {
       expect(fakeFn.handleBodyPartClick.calledWith('shoulder')).to.be.true
     })
 
-    describe('after 2 seconds', async () => {
-      await delay(2000)
-
+    describe('on stage 4', () => {
       it('should hide touch point', () => {
+        vf.handleOnBoardingClick()
         expect(fakeFn.hideGroup.calledWith('mini_onboarding_touch')).to.be.true
       })
 
@@ -104,4 +101,103 @@ describe('VisualFilter Onboarding', () => {
       })
     })
   })
+
+  after(() => {
+    contructorStub.restore()
+    restoreFakeStubs()
+    VisualFilter.__ResetDependency__('setTimeout')
+  })
 })
+
+describe('Visual Filter', () => {
+  let vf = null
+  let contructorStub
+  let fakeFn = {
+    showGroup: sinon.spy(),
+    hideGroup: sinon.spy()
+  }
+
+  before(() => {
+    contructorStub = sinon.stub(VisualFilter, 'constructor').callsFake(() => {})
+    stubFakeFunctions(VisualFilter, fakeFn)
+    vf = new VisualFilter()
+    vf.currentPropState = {
+      collar: 1,
+      coretype: 2,
+      neckline: 0,
+      shoulder: 0,
+      sleeve_length: 5,
+      top_length: 1
+    }
+  })
+
+  /**
+     * Special handling for tank top
+     * ---
+     * When coretype is moving to 0, change top_length to 0 (save current top_length value)
+     * When coretype is moving to 1 / 2 / 3 / all, restore saved top_length value
+     * When top_length is moving to 1 / 2 / 3 / all and core type is is 0, change coretype to 1
+     */
+  describe('When coretype is moving to 0', () => {
+    it('show top_length 0', () => {
+      vf.changePropSelection('coretype', '0', false)
+      expect(vf.currentPropState.top_length.toString()).to.equal('0')
+      expect(vf.showGroup.calledWith('length_0')).to.be.true
+    })
+    it('hide previous top_length selection group', () => {
+      expect(fakeFn.hideGroup.calledWith('length_1')).to.be.true
+    })
+    it('save previous top_length value', () => {
+      expect(vf.savedTopLength.toString()).to.equal('1')
+    })
+  })
+
+  describe('When coretype is moving away from 0 to 1 / 2 / 3 / all', () => {
+    it('restore previous saved top_length value', () => {
+      vf.changePropSelection('coretype', '1', false)
+      expect(vf.currentPropState.top_length.toString()).to.equal('1')
+      expect(fakeFn.showGroup.calledWith('length_1')).to.be.true
+    })
+  })
+
+  describe('When top_length is moving to 1 / 2 / 3 / all and core type is is 0', () => {
+    it('show coretype 1', () => {
+      vf.currentPropState.coretype = 0
+      vf.currentPropState.top_length = 0
+
+      vf.changePropSelection('top_length', '1', false)
+      expect(fakeFn.showGroup.calledWith('top_core_1')).to.be.true
+    })
+    it('should hide previous coretype group', () => {
+      expect(fakeFn.hideGroup.calledWith('top_core_0')).to.be.true
+    })
+  })
+
+  after(() => {
+    contructorStub.restore()
+    restoreFakeStubs()
+  })
+})
+
+let fakeFnStub = {}
+
+/**
+ * stub VisualFilter with all function from fakeFn
+ * @param {Object} target
+ * @param {Object} fakeFn
+ */
+const stubFakeFunctions = (target, fakeFn) => (
+  each(fakeFn, (spy, name) => {
+    fakeFnStub[name] = sinon.stub(target.prototype, name).callsFake(spy)
+  })
+)
+
+/**
+ * stub VisualFilter with all function from fakeFn
+ * @param {Object} fakeFn
+ */
+const restoreFakeStubs = (fakeFn) => (
+  each(fakeFnStub, (stub) => {
+    stub.restore()
+  })
+)
