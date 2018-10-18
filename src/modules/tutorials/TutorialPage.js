d7957d0fbfcbd407f941f790f2471cdc0770bdb4
Sukjae Cho <sukjae.cho@gmail.com>
2018-10-18 10:34:51 -0700

    Reduced tutorial steps. Fixed layouts for small screen size. Changed back button to replay in tutorial

--- src/modules/tutorials/TutorialPage.js
+++ src/modules/tutorials/TutorialPage.js
@@ -31,12 +31,21 @@ export default class TutorialPage extends Component {
   }
 
   componentWillUnmount () {
-    this.pageTimeout && clearTimeout(this.pageTimeout)
+    if (this.pageTimeout) {
+      clearTimeout(this.pageTimeout)
+    }
   }
 
   render () {
     const { pageKey, activeKey, children, onClick } = this.props
 
+    if (pageKey !== activeKey) {
+      // componentWillUnmount is not called when replay is clicked.
+      // Stop timer not to advance to next page with previous timer.
+      if (this.pageTimeout) {
+        clearTimeout(this.pageTimeout)
+      }
+    }
     return (
       <Transition timeout={{ enter: 500, exit: 500 }} show={pageKey === activeKey} transition='fadeIn' onEntered={this.handleEntered}>
         <div className='TutorialPage' onClick={onClick}>
